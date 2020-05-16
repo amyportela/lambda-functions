f39fe9079cd3a368b02ba7f413eb4544d2573f35
Amy Portela <58912682+amyportela@users.noreply.github.com>
2020-05-15 23:24:54 -0300

    Pymongo Script
    Connection and find on mongodb via lambda using pymongo

---
+++ mongodb-connection-pymongo.py
@@ -0,0 +1,27 @@
+import json
+from core import logger
+from pymongo import MongoClient #necessário deploy de package 
+from bson.json_util import dumps
+
+from core.config.mongo import settings
+
+def get_data():
+
+    logger.info("Getting data from mongo-db")
+    db_name = "dbname"
+
+    print("Iniciando conexao com o MongoDB, db={}".format(db_name))
+
+    mongo_user = settings["mongo_user"]
+    mongo_password = settings["mongo_password"]
+    mongo_server = settings["mongo_server"]
+    mongo_uri = f'mongodb://{mongo_user}:{mongo_password}@{mongo_server}/{db_name}'
+
+    client = MongoClient(mongo_uri)
+    db = client[db_name]
+    find = db.collection.find(no_cursor_timeout=True)
+    result = dumps(find)
+    client.close()
+
+    return json.loads(result)
+
